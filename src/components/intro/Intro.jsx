bf62138a23a6de5a31a4ef69e3f6b6a4472ecb76
nampn.nh <nam.nguyen3@hitachivantara.com>
2021-10-31 23:44:27 +0700

    start project

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,17 @@
+import './intro.css'
+
+function Intro() {
+    return (
+        <div className="i">
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                    <h2 className="i-intro">Hello, My name is</h2>
+                    <h1 className="name">Nguyen Phuong Nam</h1>
+                </div>
+            </div>
+            <div className="i-right">right</div>
+        </div>
+    )
+}
+
+export default Intro
